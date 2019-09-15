c622072efa64636053d11b914875f79ec9538d24
fehead <im.fehead@gmail.com>
2019-09-15 23:02:14 +0900

    add board contents.

--- src/main/java/im/fehead/SpringBootOAuth2Application.java
+++ src/main/java/im/fehead/SpringBootOAuth2Application.java
@@ -2,6 +2,7 @@ package im.fehead;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -11,7 +12,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import im.fehead.domain.Board;
 import im.fehead.domain.User;
+import im.fehead.domain.enums.BoardType;
 import im.fehead.repository.BoardRepository;
 import im.fehead.repository.UserRepository;
 import im.fehead.resolver.UserArgumentResolver;
@@ -35,12 +38,23 @@ public class SpringBootOAuth2Application extends WebMvcConfigurerAdapter{
 	public CommandLineRunner runner(UserRepository userRepository,
 			BoardRepository boardRepository) {
 		return (args) -> {
-			userRepository.save(User.builder()
+			User user = User.builder()
 				.name("havi")
 				.email("test")
 				.pincipal("havi@gmail.com")
 				.createdDate(LocalDateTime.now())
-				.build()
+				.build();
+			userRepository.save(user);
+			
+			IntStream.rangeClosed(1, 200).forEach(index ->
+				boardRepository.save(Board.builder()
+					.title("게시글 "+index)
+					.subTitle("순서 "+index)
+					.content("내용~~~")
+					.boardType(BoardType.free)
+					.createdDate(LocalDateTime.now())
+					.updatedDate(LocalDateTime.now())
+					.user(user).build())
 			);
 		};
 	}
