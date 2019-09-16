4ba5ae0356746bbf1967cf23c77bdce6bc4a7e4a
fehead <im.fehead@gmail.com>
2019-09-16 23:59:10 +0900

    add BoardService.

---
+++ src/main/java/im/fehead/service/BoardService.java
@@ -0,0 +1,32 @@
+package im.fehead.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import im.fehead.domain.Board;
+import im.fehead.repository.BoardRepository;
+
+/**
+ * Created by KimYJ on 2017-07-13.
+ */
+@Service
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    public BoardService(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+    public Page<Board> findBoardList(Pageable pageable) {
+        pageable = PageRequest.of(pageable.getPageNumber() <= 0 ? 0 : pageable.getPageNumber() - 1, pageable.getPageSize());
+        return boardRepository.findAll(pageable);
+    }
+
+    public Board findBoardByIdx(Long idx) {
+        return boardRepository.findById(idx).orElse(new Board());
+    }
+
+}
