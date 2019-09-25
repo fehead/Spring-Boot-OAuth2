ce00fd1bd0451bb09065842597bdc2bbf0cf09c9
fehead <im.fehead@gmail.com>
2019-09-25 23:41:44 +0900

    add LiternalTest test case.

--- src/test/java/im/fehead/LiternalTest.java
+++ src/test/java/im/fehead/LiternalTest.java
@@ -13,6 +13,19 @@ public class LiternalTest {
 		Integer	a = 10;
 		Integer b = new Integer(10);
 		Integer c = 10;
+		int s = b;
+		
+		log.info("a : {}, b {}, c {}", a, b, c);
+		log.info("a.equals(b) {}", a.equals(b));
+		log.info("a.equals(b) {}", a.equals(b));
+		log.info("a == c {}", a == c);
+		log.info("a == b {}", a == b);
+		
+		log.info("&a {}, &b {}, &c {}, &s {}", System.identityHashCode(a)
+				, System.identityHashCode(b)
+				, System.identityHashCode(c)
+				, System.identityHashCode(s)
+				);
 		
 		assertThat(a.equals(b)).isTrue();
 		assertThat(a == c).isTrue();
