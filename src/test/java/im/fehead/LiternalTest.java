b50d7b692e9987a1431cf8142b3c0696a6eb55fe
fehead <im.fehead@gmail.com>
2019-09-27 23:54:14 +0900

    ..

--- src/test/java/im/fehead/LiternalTest.java
+++ src/test/java/im/fehead/LiternalTest.java
@@ -10,23 +10,20 @@ import lombok.extern.slf4j.Slf4j;
 public class LiternalTest {
 	@Test
 	public void integerTest() {
-		Integer	a = 10;
+		Integer a = 10;
 		Integer b = new Integer(10);
 		Integer c = 10;
 		int s = b;
-		
+
 		log.info("a : {}, b {}, c {}", a, b, c);
 		log.info("a.equals(b) {}", a.equals(b));
 		log.info("a.equals(b) {}", a.equals(b));
 		log.info("a == c {}", a == c);
 		log.info("a == b {}", a == b);
-		
-		log.info("&a {}, &b {}, &c {}, &s {}", System.identityHashCode(a)
-				, System.identityHashCode(b)
-				, System.identityHashCode(c)
-				, System.identityHashCode(s)
-				);
-		
+
+		log.info("&a {}, &b {}, &c {}, &s {}", System.identityHashCode(a), System.identityHashCode(b),
+				System.identityHashCode(c), System.identityHashCode(s));
+
 		assertThat(a.equals(b)).isTrue();
 		assertThat(a == c).isTrue();
 		assertThat(a == b).isFalse();
